c1a32f4a4598451b26c1da78cdcd5496f76a9704
virBeaver <tim.maier@icloud.com>
2019-08-19 09:39:43 +0200

    Improved commenting

--- New-AutomatedAsBuiltReport.ps1
+++ New-AutomatedAsBuiltReport.ps1
@@ -17,29 +17,31 @@
     Year:       2019
 #>
 
-#Set Variables
+#Set variables
 $OutputPath = ".\"
 $AsBuiltConfigPath = ".\Config\AsBuiltReport.Config.json"
 
 
-#Trust repositroy "PSGallery"
+#Trust repository "PSGallery"
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 
-#Check if VMware.PowerCLI are installed
+#Check if VMware.PowerCLI is installed
 if (!(Get-Module -Name VMware.PowerCLI -ListAvailable)) {
     Install-Module -Name VMware.PowerCLI -Force
 } else {
     Update-Module -Name VMware.PowerCLI
 }
 
-#Check if AsBuiltReport are installed
+#Check if AsBuiltReport is installed
 if (!(Get-Module -Name AsBuiltReport -ListAvailable)) {
     Install-Module -Name AsBuiltReport -Force
 } else {
     Update-Module -Name AsBuiltReport 
 }
 
+#Asked user for input (vCenter Server and credentials)
 $VIServer = Read-Host -Prompt "vCenter Server to connect to"
 $VICredential = Get-Credential -Message "Please enter the credentials for $VIServer"
 
+#Run AsBuiltReport with predefined configuration
 New-AsBuiltReport -Target $VIServer -Credential $VICredential -Format Word  -Report VMware.vSphere -EnableHealthCheck -OutputPath $OutputPath -AsBuiltConfigPath $AsBuiltConfigPath
